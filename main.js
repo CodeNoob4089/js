ad5d6bc575c9b0775178037ca2f2d3d56290e71a
bongho <wqoo2190@gmail.com>
2023-06-05 11:30:43 +0900

    prettier

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MThiNWFlOTkyMWVhMDg3NDA3Yjc0ZGE0N2M4YjkwZSIsInN1YiI6IjY0NzQxYWYwOTQwOGVjMDBlMTRkMjIxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uhe1VABbC4KF0emSaa_XiCC-VvcoKEXHKc9lyAGEqKk'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MThiNWFlOTkyMWVhMDg3NDA3Yjc0ZGE0N2M4YjkwZSIsInN1YiI6IjY0NzQxYWYwOTQwOGVjMDBlMTRkMjIxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uhe1VABbC4KF0emSaa_XiCC-VvcoKEXHKc9lyAGEqKk",
+  },
 };
 
 let movies = [];
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(data => {
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((data) => {
     movies = data.results;
     displayMovies(movies);
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
 function displayMovies(movies) {
   const movieContainer = document.getElementById("movieContainer");
   movieContainer.innerHTML = "";
 
-  movies.forEach(movie => {
+  movies.forEach((movie) => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
@@ -29,15 +33,15 @@ function displayMovies(movies) {
         <p>내용 요약: ${movie.overview}</p>
         <p class="rating">평점: ${movie.vote_average}</p>
       `;
-      card.setAttribute("data-id",movie.id);
-      card.addEventListener('click',function(event) {
-        event.currentTarget.getAttribute('data-id');
-        const id = event.currentTarget.getAttribute('data-id');
-        alert(`id = ${id}`);
-      });
+    card.setAttribute("data-id", movie.id);
+    card.addEventListener("click", function (event) {
+      event.currentTarget.getAttribute("data-id");
+      const id = event.currentTarget.getAttribute("data-id");
+      alert(`id = ${id}`);
+    });
     movieContainer.appendChild(card);
   });
-};
+}
 
 function filter() {
   let input = document.getElementById("searchInput").value.toLowerCase();
